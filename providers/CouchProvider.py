119d468676eb082ec6fb35005e80b792a49fb0ce
Ryan Gordon <ryangordon210@gmail.com>
2018-01-29 13:06:18 +0000

    Added file changes

--- providers/CouchProvider.py
+++ providers/CouchProvider.py
@@ -5,9 +5,8 @@ product = {
         "catagory": "catagory1",
         "quantity": "4"
         }
+
 class CouchProvider(object):
     
     def read_product(self) -> str:
         return product,200
-
-    
